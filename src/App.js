1beb9511261f09b0983c0dc704271d0a1c2d630a
Herimalala Ranaivoarisoa Rivomanana <ranaivoarisoa@gmx.fr>
2022-03-12 08:15:49 +0300

    [TEST]

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
       <div>
-        <button type='submit' id='add-product-btn'>ADD</button>
-        <button type='submit' id='delete-product-btn' >MASS DELETE</button>
+        <button id='add-product-btn'>ADD</button>
+        <button id='delete-product-btn'>MASS DELETE</button>
       </div>
     </div>
   );
