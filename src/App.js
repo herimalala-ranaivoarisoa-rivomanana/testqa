db51f732aed14ea8060e47205eec656c041aa949
Herimalala Ranaivoarisoa Rivomanana <ranaivoarisoa@gmx.fr>
2022-03-12 07:58:47 +0300

    [TEST]

--- src/App.js
+++ src/App.js
@@ -4,20 +4,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <button type='submit' id='add-product-btn'>ADD</button>
+        <button type='submit' id='delete-product-btn' >MASS DELETE</button>
+      </div>
     </div>
   );
 }
